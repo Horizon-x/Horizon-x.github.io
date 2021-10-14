ec78f4f2b2e5f853ae336c9c4d7d9496bc8bb7b4
xxx <xiangjianxinxxx@163.com>
2021-10-14 15:41:25 +0800

    Update Hexo Static Content

---
+++ js/shuoshuo/shuoshuo.js
@@ -0,0 +1,27 @@
+new Artitalk({
+    serverURL: 'https://spring-paper-4d55.onexstone.workers.dev',
+    appId: 'dfaskjfhlsaf-asdfsa',
+    appKey: 'safuoghksahdfasf',
+    pageSize: 8,
+    shuoPla: '记录下今天的好心情！',
+    avatarPla: 'https://cdn.jsdelivr.net/gh/horizon-x/imagesCDN/img/avatar-auther.png',
+    atComment: 0,
+    // motion: 0,
+    atEmoji: {
+                baiyan: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/baiyan.png",
+                bishi: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/bishi.png",
+                bizui: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/bizui.png",
+                chan: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/chan.png",
+                daku: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/daku.png",
+                dalao: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/dalao.png",
+                dalian: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/dalian.png",
+                dianzan: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/dianzan.png",
+                doge: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/doge.png",
+                facai: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/facai.png",
+                fadai: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/fadai.png",
+                fanu: "https://cdn.jsdelivr.net/gh/Artitalk/Artitalk-emoji/fanu.png",
+    },
+})
+
+// window.location.reload();
+
